77dde11befb30588a932f41071c5e8da7c798a6e
elkurto <kurt.buehner@gmail.com>
2025-04-20 15:48:59 -0400

    first commit

---
+++ 00-go-fmt/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func doodle() {
+	s := "gopher"
+	fmt.Printf("Hello and welcome, %s!\n", s)
+
+	for i := 1; i <= 5; i++ {
+		fmt.Println("i =", 100/i)
+	}
+} //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+
+func main() {
+	doodle()
+}
