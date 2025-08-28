996c7a6bdfcbbe55ba1d3f867e966100a83f5575
elkurto <kurt.buehner@gmail.com>
2025-08-28 18:38:26 -0400

    ad_ad_go_struct2 dodgy but working solution

--- ad_ad_go_struct2/go.mod
+++ ad_ad_go_struct2/go.mod
@@ -1,3 +1,3 @@
-module ad_ad_struct02
+module ad_ad_go_struct2
 
 go 1.25.0
--- ad_ad_go_struct2/main.go
+++ ad_ad_go_struct2/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"ad_ad_go_struct2/music_catalog"
+	"fmt"
+)
+
+func createSampleData() []*music_catalog.Song {
+	m1 := music_catalog.NewMusician(1, "Hiatt")
+	m2 := music_catalog.NewMusician(2, "DjMarsh")
+
+	b3 := music_catalog.NewBand(3, "Hiatt and the Combo")
+	b4 := music_catalog.NewBand(4, "DJ Marsh")
+
+	mb13 := music_catalog.NewBandMusician(5, *b3, *m1)
+	mb24 := music_catalog.NewBandMusician(6, *b4, *m2)
+
+	var listSong = []*music_catalog.Song{
+		music_catalog.NewSong(1, "Master Of Disaster", []music_catalog.BandMusician{*mb13}),
+		music_catalog.NewSong(2, "Live1", []music_catalog.BandMusician{*mb24}),
+	}
+
+	return listSong
+}
 
 func main() {
 	fmt.Println("hello")
+	listPtrSong := createSampleData()
+
+	for _, songPtr := range listPtrSong {
+
+	}
+
 }
